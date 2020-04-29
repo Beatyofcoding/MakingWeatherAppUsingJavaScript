5f7f56716ccc0f0e4876ee4efa1b11f4bba2f116
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-29 23:43:08 +0200

    Starting with Javascript File( Selecting Elements)

---
+++ script.js
@@ -0,0 +1,7 @@
+// Selecting Elements
+
+cons iconElement = document.querySelector(".weather-icon");
+cons tempElement = document.querySelector(".temperature-value p");
+cons descElement = document.querySelector(".temperature-description p");
+cons locationElement = document.querySelector(".location p");
+cons notificationElement = document.querySelector(".notification");
