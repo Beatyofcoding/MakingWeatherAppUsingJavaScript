2e8fe6abac2abc4925ce760e67a1eaa58b956ca9
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-04-30 18:11:35 +0200

    Fixing Tiny syntax error

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Selecting Elements
 
-cons iconElement = document.querySelector(".weather-icon");
-cons tempElement = document.querySelector(".temperature-value p");
-cons descElement = document.querySelector(".temperature-description p");
-cons locationElement = document.querySelector(".location p");
-cons notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
 
 
 
