311748971ca6e5eba458f74c996c8fa78737c6f9
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-05-01 18:38:28 +0200

    Adding Func that Displays Weather To UI

--- script.js
+++ script.js
@@ -64,3 +64,9 @@ function getWeather(latitude, longitude){
 }
 
 // Display Weather To UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
