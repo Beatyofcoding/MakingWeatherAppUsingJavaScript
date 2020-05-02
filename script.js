8e824c7d104db17df469f17823211d350477e9ee
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-05-02 19:59:34 +0200

    AddingEventListenerToMakeUserChangigTempUnitByClik

--- script.js
+++ script.js
@@ -75,3 +75,19 @@ function displayWeather(){
 function celsiusToFahrenheit(temperature){
     return (temperature * 9/5) + 32;
 }
+
+// Adding EventListener So When the user clicks on the temperature element it excute the code
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.unit == undefined) 
+    return;
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weatù.temperature.unit = "celsius"
+    }
+});
